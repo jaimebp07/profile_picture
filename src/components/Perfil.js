e1fd83c50fdc1122c3b0478ffc798eb5c4592254
jbaron5bdb <jbaron5@bancodebogota.com.co>
2023-03-26 14:57:08 -0500

    ci: probando el pipeline

--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -5,7 +5,7 @@ function Perfil() {
     return (
         <div className='content-perfil'>
             <img className='img-perfil'
-                src='https://scontent.feyp2-1.fna.fbcdn.net/v/t1.6435-9/212796073_2962788900610419_7163004677526732900_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=teoJwET_N0MAX8Byc1E&_nc_ht=scontent.feyp2-1.fna&oh=00_AfAkKhe2drfQdmQpyJKAr07yVt7sZZmBtplMz2yV2oTB1A&oe=63EB888B' 
+                src='https://scontent.feyp2-1.fna.fbcdn.net/v/t1.6435-9/212796073_2962788900610419_7163004677526732900_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=FT6iJoP9d3AAX8NQD7C&_nc_ht=scontent.feyp2-1.fna&oh=00_AfClMgjTh0pLSznMnsAuJ9b85CF8d9NRtMiHkQIyjlaLTQ&oe=6447D18B' 
                 alt='foto de Andres'/>
             <div className='container-description'>
                 <p className='name'>Jaime Andres Baron Puin</p>
